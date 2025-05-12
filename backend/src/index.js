a9f2ed985a8632cb3278226d169f38789f03ce0d
Abhishek Singh <81.fycs@gmail.com>
2025-05-12 17:30:53 +0530

    index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ app.use(cookieparser());
 // secure url path by providing url
 app.use(
     cors({ // only allow this url to access only other are block
-        origin:["https://chat-app-frontend-exw3.onrender.com" ],
+        origin:["https://chat-app-frontend-exw3.onrender.com/" ],
         methods:["POST","GET","PUT","DELETE"],
         credentials:true,
     })
