896f118b98cc7aab7b832a7e27b02979c1341552
Abhishek Singh <81.fycs@gmail.com>
2025-05-12 17:09:17 +0530

    axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
 // valify the backend when we in development mode then use url either in production then user /api
-  baseURL:"https://chat-app-backend-ia1r.onrender.com",
+  baseURL:"https://chat-app-backend-ia1r.onrender.com/api",
   timeout: 5000,
   withCredentials: true,  // credential is true to share cookies
 
